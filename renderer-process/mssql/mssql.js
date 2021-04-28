ff1abbf224095822dcda44d0dfc3168d132be414
chowyu12 <chowyu12@163.com>
2021-04-28 15:42:29 +0800

    add query

--- renderer-process/mssql/mssql.js
+++ renderer-process/mssql/mssql.js
@@ -7,6 +7,9 @@ queryMsgBtn.addEventListener('click', () => {
     // const message = `mysql-query message reply: ${reply}`
     window.query('ss', function (result) {
         document.getElementById('mysql-query-reply').innerHTML = JSON.stringify(result)
+        for (var i = 0; i < result.length; i++) {
+            $("#mssql-table tbody").append($("<tr><td>" + result[i].user + "</td><td>" + result[i].password + "</td><td>" + result[i].score + "</td><td>" + result[i].type + "</td></tr>"));
+        }
     })
 
 })
